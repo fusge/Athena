8b0a5004ec557b89f5fcf380bdd94f86107d5f66
Ivan Valerio <ivanvalerio@hotmail.com>
2020-11-30 23:01:33 -0500

    Major updates to the game logic and internal rules

--- chesspeer/chessboard.cpp
+++ chesspeer/chessboard.cpp
@@ -20,7 +20,7 @@ void chesspeer::chessboard::set_board(std::string fen) {
 	std::array<char, 64>::iterator board_iter = this->board.begin();
 	std::string::iterator fen_iter = fen.begin();
 	int emptysquares;
-	struct chesspeer::Movenode* position = new chesspeer::Movenode();
+	struct chesspeer::Movenode* gameTree = new chesspeer::Movenode();
 
 	// First store the board pieces
 	for(board_iter; board_iter != this->board.end(); board_iter++) {
@@ -40,17 +40,17 @@ void chesspeer::chessboard::set_board(std::string fen) {
 
 	// Handle whos move is it anyway
 	fen_iter++;
-	position->color_to_move = *fen_iter;
+	gameTree->color_to_move = *fen_iter;
 	fen_iter++;
 
 	// Handle castling rights
 	fen_iter++;
 	while (*fen_iter != ' ') {
 		switch (*fen_iter) {
-		case 'K': position->white_castle_kingside = true;
-		case 'Q': position->white_castle_queenside = true;
-		case 'k': position->black_castle_kingside = true;
-		case 'q': position->black_castle_queenside = true;
+		case 'K': gameTree->white_castle_kingside = true;
+		case 'Q': gameTree->white_castle_queenside = true;
+		case 'k': gameTree->black_castle_kingside = true;
+		case 'q': gameTree->black_castle_queenside = true;
 		default: fen_iter++;
 		}
 	}
@@ -58,11 +58,11 @@ void chesspeer::chessboard::set_board(std::string fen) {
 	// store en_passant squares
 	fen_iter++;
 	if (*fen_iter != '-') {
-		position->en_passant = std::string(fen_iter, fen_iter + 2);
+		gameTree->en_passant = std::string(fen_iter, fen_iter + 2);
 		fen_iter = fen_iter + 3;
 	}
 	else {
-		position->en_passant = *fen_iter;
+		gameTree->en_passant = *fen_iter;
 		fen_iter = fen_iter + 2;
 	}
 
@@ -71,12 +71,12 @@ void chesspeer::chessboard::set_board(std::string fen) {
 	while (*fen_iter != ' ') {
 		fen_iter++;
 	}
-	position->plys_since_capture = std::stoi(std::string(start_digit, fen_iter));
+	gameTree->plys_since_capture = std::stoi(std::string(start_digit, fen_iter));
 
 	fen_iter++;
-	position->on_move = std::stoi(std::string(fen_iter, fen.end()));
+	gameTree->on_move = std::stoi(std::string(fen_iter, fen.end()));
 
-	this->position = position;
+	this->gameTree = gameTree;
 }
 
 void chesspeer::chessboard::show() {
@@ -94,8 +94,8 @@ void chesspeer::chessboard::show() {
 		counter++;
 	}
 	std::cout << std::endl;
-	std::cout << this->position->color_to_move << " to move" << std::endl;
-	std::cout << "on move " << this->position->on_move << std::endl;
+	std::cout << this->gameTree->color_to_move << " to move" << std::endl;
+	std::cout << "on move " << this->gameTree->on_move << std::endl;
 }
 
 std::string chesspeer::chessboard::get_fen() {
@@ -128,33 +128,159 @@ std::string chesspeer::chessboard::get_fen() {
 	}
 
 	fen += ' ';
-	fen += this->position->color_to_move;
+	fen += this->gameTree->color_to_move;
 	fen += ' ';
 
-	if (this->position->white_castle_kingside == true) {
+	if (this->gameTree->white_castle_kingside == true) {
 		fen += 'K';
 	}
-	if (this->position->white_castle_queenside == true) {
+	if (this->gameTree->white_castle_queenside == true) {
 		fen += 'Q';
 	}
-	if (this->position->black_castle_kingside == true) {
+	if (this->gameTree->black_castle_kingside == true) {
 		fen += 'k';
 	}
-	if (this->position->black_castle_queenside == true) {
+	if (this->gameTree->black_castle_queenside == true) {
 		fen += 'q';
 	}
 
 	fen += ' ';
-	fen += this->position->en_passant;
+	fen += this->gameTree->en_passant;
 	fen += ' ';
 
 	char nums[4];
-	_itoa(this->position->plys_since_capture, nums, 10);
+	_itoa(this->gameTree->plys_since_capture, nums, 10);
 	fen.append(nums);
 	fen += ' ';
-	_itoa(this->position->on_move, nums, 10);
+	_itoa(this->gameTree->on_move, nums, 10);
 	fen.append(nums);
 
 	return fen;
+}
+
+bool chesspeer::chessboard::_checkbounds(int row, int col) {
+	return ((row >= 0) && (row < 8) && (col >= 0) && (col < 8));
+}
+
+int chesspeer::chessboard::_coordtoindex(std::string coordinates) {
+	int col = int(coordinates[0]) - 97;
+	int row = 8 - (int(coordinates[1]) - 48);
+	int boardindex = (row * 8) + col;
+	return boardindex;
+}
+
+std::vector<int> chesspeer::chessboard::_knightMoves(std::string square) {
+	int index = this->_coordtoindex(square);
+	std::vector<int> indices;
+	int row = index / 8;
+	int col = index % 8;
+	for (int rowdiff = -2; rowdiff <= 2; rowdiff++) {
+		for (int coldiff = -2; coldiff <= 2; coldiff++) {
+			if ((coldiff != rowdiff) && (abs(coldiff) != abs(rowdiff)) && (coldiff != 0 || rowdiff != 0)) {
+				if (this->_checkbounds(row+rowdiff, col+coldiff)) {
+					indices.push_back(((row + rowdiff) * 8) + (col + coldiff));
+				}
+			}
+		}
+	}
+	return indices;
+}
+
+std::vector<int> chesspeer::chessboard::_kingMoves(std::string square) {
+	int index = this->_coordtoindex(square);
+	std::vector<int> indices;
+	int row = index / 8;
+	int col = index % 8;
+	for (int rowdiff = -1; rowdiff <= 1; rowdiff++) {
+		for (int coldiff = -1; coldiff <= 1; coldiff++) {
+			if ((coldiff != 0) || (rowdiff != 0)) {
+				if (this->_checkbounds(row+rowdiff, col+coldiff)) {
+					indices.push_back(((row + rowdiff) * 8) + (col + coldiff));
+				}
+			}
+		}
+	}
+	return indices;
+}
+
+std::vector<int> chesspeer::chessboard::_bishopMoves(std::string square) {
+	int index = this->_coordtoindex(square);
+	std::vector<int> indices;
+	int row = index / 8;
+	int col = index % 8;
+	for (int rowdiff = -7; rowdiff <= 7; rowdiff++) {
+		if (rowdiff != 0) {
+			if (this->_checkbounds(row+rowdiff, col+rowdiff)) {
+				indices.push_back(((row + rowdiff) * 8) + (col + rowdiff));
+			}
+		}
+	}
+	return indices;
+}
+
+std::vector<int> chesspeer::chessboard::_queenMoves(std::string square) {
+	int index = this->_coordtoindex(square);
+	std::vector<int> indices;
+	int row = index / 8;
+	int col = index % 8;
+	for (int rowdiff = -7; rowdiff <= 7; rowdiff++) {
+		for (int coldiff_mult = -1; coldiff_mult <= 1; coldiff_mult++) {
+			if (rowdiff != 0 || coldiff_mult != 0) {
+				if (this->_checkbounds(row+rowdiff, col+(rowdiff*coldiff_mult))) {
+					indices.push_back(((row + rowdiff) * 8) + (col + (rowdiff * coldiff_mult)));
+				}
+			}
+		}
+	}
+	return indices;
+}
+
+std::vector<int> chesspeer::chessboard::_rookMoves(std::string square) {
+	int index = this->_coordtoindex(square);
+	std::vector<int> indices;
+	int row = index / 8;
+	int col = index % 8;
+	for (int indxdiff = -7; indxdiff <= 7; indxdiff++) {
+		if (indxdiff != 0 ) {
+			if (this->_checkbounds(row+indxdiff, col)) {
+				indices.push_back(((row + indxdiff) * 8) + col);
+			}
+			if (this->_checkbounds(row, col+indxdiff)) {
+				indices.push_back((row * 8) + (col + indxdiff));
+			}
+		}
+	}
+	return indices;
+}
+
+std::vector<int> chesspeer::chessboard::_pawnMoves(std::string square, char color) {
+	int index = this->_coordtoindex(square);
+	std::vector<int> indices;
+	int row = index / 8;
+	int col = index % 8;
+	if (color == 'w') {
+		indices.push_back(((row - 1) * 8) + col);
+		if (this->_checkbounds(row - 1, col+1)) {
+			indices.push_back(((row - 1) * 8) + col + 1);
+		}
+		if (this->_checkbounds(row - 1, col - 1)) {
+			indices.push_back(((row - 1) * 8) + col - 1);
+		}
+	}
+	else if (color == 'b') {
+		indices.push_back(((row + 1) * 8) + col);
+		if (this->_checkbounds(row + 1, col + 1)) {
+			indices.push_back(((row + 1) * 8) + col + 1);
+		}
+		if (this->_checkbounds(row + 1, col - 1)) {
+			indices.push_back(((row + 1) * 8) + col - 1);
+		}
+	}
+	return indices;
+}
+
+bool chesspeer::chessboard::isValidMove(std::string move) {
+	std::string::iterator iter = move.begin();
+	
 }
 
--- chesspeer/chessboard.h
+++ chesspeer/chessboard.h
@@ -9,7 +9,7 @@
 namespace chesspeer {
 	struct Movenode {
 		std::string value;
-		unsigned int ply;
+		int ply;
 		char color_to_move;
 		char captured_piece;
 		std::string en_passant;
@@ -22,21 +22,30 @@ namespace chesspeer {
 		struct Movenode* prevmove;
 		struct Movenode* nextmove;
 		std::map<std::string,struct Movenode*> sidelines;
-		Movenode();
 	};
 
 	class chessboard {
 	private:
 		std::array<char, 64> board;
-		struct Movenode* position;
-
+		struct Movenode* gameTree;
+		struct Movenode* currentPosition;
+		
+		int _coordtoindex(std::string coordinates);
+		bool _checkbounds(int row, int col);
+		std::vector<int> _knightMoves(std::string square);
+		std::vector<int> _bishopMoves(std::string square);
+		std::vector<int> _rookMoves(std::string square);
+		std::vector<int> _queenMoves(std::string square);
+		std::vector<int> _kingMoves(std::string square);
+		std::vector<int> _pawnMoves(std::string square, char color);
 	public:
 		chessboard ();
 		chessboard (std::string fen);
 
 		std::string get_fen();
 		void set_board(std::string fen);
-		void set_pieces_style();
+		bool isValidMove(std::string move);
+		void play_move(std::string move);
 		std::vector<std::string> get_moves();
 
 		void show();
