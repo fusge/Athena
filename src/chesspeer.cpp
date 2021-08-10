d273caf778a8c9ea5ce4daec9e652fb52ef5e219
Ivan F. Valerio <valerioif@gmail.com>
2021-08-09 21:35:52 -0400

    Properly configure cmake to compile out of source binaries

--- src/chesspeer.cpp
+++ src/chesspeer.cpp
@@ -1,11 +1,18 @@
 ﻿// chesspeer.cpp : Defines the entry point for the application.
 //
 
-#include "chesspeer.h"
+//#include "chesspeer.h"
 #include "chessgame.h"
+#include "src/ChesspeerConfig.h"
+#include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
+    std::cout << *argv[0] << " VERSION " ;
+    std::cout << CHESSPEER_VERSION_MAJOR << ".";
+    std::cout << CHESSPEER_VERSION_MINOR << ".";
+    std::cout << CHESSPEER_VERSION_PATCH << std::endl;
+
 	auto mychessboard = chesspeer::chessgame();
 	mychessboard.show();
 	std::cout << mychessboard.get_fen() << std::endl;
