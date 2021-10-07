6f0115ff21fe44472e440bf02fcf2f0038230156
Ivan F. Valerio <valerioif@gmail.com>
2021-10-06 21:19:40 -0400

    added version information to about menu option

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1,5 +1,10 @@
 #include "main_window.h"
+#include "chessgame.h"
+#include "ChesspeerConfig.h"
+#include <string>
 
+#include <wx/msgout.h>
+#include <wx/string.h>
 
 enum
 {
@@ -33,16 +38,26 @@ UI::ChessBoard::ChessBoard()
     Bind(wxEVT_MENU, &UI::ChessBoard::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &UI::ChessBoard::OnExit, this, wxID_EXIT);
 }
+
 void UI::ChessBoard::OnExit(wxCommandEvent& event)
 {
     Close(true);
 }
+
 void UI::ChessBoard::OnAbout(wxCommandEvent& event)
 {
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
+    std::string version = "Chesspeer version ";
+    version += std::to_string(CHESSPEER_VERSION_MAJOR);
+    version += ".";
+    version += std::to_string(CHESSPEER_VERSION_MINOR);
+    version += (".");
+    version += std::to_string(CHESSPEER_VERSION_PATCH);
+    
+    wxString message (version);
+    wxMessageBox(message , "About Chesspeer", wxOK | wxICON_INFORMATION);
 }
+
 void UI::ChessBoard::OnHello(wxCommandEvent& event)
 {
-    wxLogMessage("Hello world from wxWidgets!");
+    wxLogMessage("Chesspeer UI loadded");
 }
